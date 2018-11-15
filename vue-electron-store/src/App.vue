defeac2b81ec9f7755216406102687cdbecc2282
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-14 19:04:22 -0600

    update

--- vue-electron-store/src/App.vue
+++ vue-electron-store/src/App.vue
@@ -5,4 +5,17 @@
   </div>
 </template>
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
 <style src="./assets/main.css">
+
+</style>
