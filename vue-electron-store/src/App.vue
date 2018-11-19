b4b8ad6afc118a759c4744352b6d7c0d369c09a5
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-19 12:21:49 -0600

    add sidebar

--- vue-electron-store/src/App.vue
+++ vue-electron-store/src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <div id="app" style="-webkit-app-region: drag" class="flex min-h-screen">
-    <div class="w-56 bg-grey-darkest text-white border-r border-blackblack fixed min-h-screen">
-      This is a Sidebar
-    </div>
-    <HelloWorld msg="Welcome to Vue"/>
+  <div id="app"
+    style="-webkit-app-region: drag"
+    class="font-sans flex min-h-screen">
+    <sidebar></sidebar>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Sidebar
   }
 }
 </script>
 
 <style src="./assets/main.css">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ vue-electron-store/src/components/Sidebar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-56 bg-grey-darkest text-white border-r border-blackblack fixed min-h-screen pt-8">
+    <div class="search-container text-center p-4 relative">
+      <input type="text" placeholder="Search" class="bg-grey-darker text-grey-light text-xs w-full p-2 pl-8 border border-grey-dark rounded">
+      <div class="absolute pin-t py-5 px-2 text-grey-light">
+        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 18" width="18" height="18"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path></svg>
+      </div>
+    </div>
+  </div>
+</template>
