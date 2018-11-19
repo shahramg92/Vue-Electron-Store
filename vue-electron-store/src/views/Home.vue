86ffb4bba8eba7b374663fe5383023171cf0fbf2
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-19 13:19:50 -0600

    add banner & updates

--- vue-electron-store/src/views/Home.vue
+++ vue-electron-store/src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="bg-black text-white flex-1 ml-56">
+    <div class="discover-banner">
+      <div class ="uppercase mb-4">The Basics</div>
+      <h1 class="font-normal mb-4">Create Personal Projects</h1>
+      <p class="w-64 leading-normal text-grey-lighter">With these apps, creativity has no boundaries.</p>
+    </div>
+
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </template>
