3eff89d2b3eca56784553491ecba480745d0eb4c
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-19 16:47:52 -0600

    finish filling out home page

--- vue-electron-store/src/views/Home.vue
+++ vue-electron-store/src/views/Home.vue
@@ -24,16 +24,201 @@
         </div>
       </div>
 
-      <div class="w-1/2 ml-4 bg-grey-darkest rounded-lg flex px-8 py-6">
-        <div class="uppercase text-grey-light text-cs mb-1">Collection</div>
+      <div class="w-1/2 ml-4 bg-grey-darkest rounded-lg px-8 py-6">
+        <div class="uppercase text-grey-light text-xs mb-1">Collection</div>
         <h1 class="font-normal text-2xl mb-4">Apps That Look Great in Dark Mode</h1>
         <p class="text-grey-light text-cs leading-normal mb-4">Dark is the new black.</p>
         <img src="../assets/icons_darkmode.png" alt="icons">
       </div>
     </div>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="border-b border-grey-darker mb-10"></div>
+
+    <div class="apps-games-we-love">
+      <div class="flex justify-between">
+        <h2 class="font-normal mb-8">Apps and Games We Love Right Now</h2>
+        <a href="#" class="text-blue no-underline">See All</a>
+      </div>
+
+    <div class="flex flex-wrap mb-4">
+      <div class="w-1/2 flex">
+        <div class="w-24">
+          <img src="../assets/icon_quark.png" alt="icon">
+        </div>
+        <div class="pl-6 flex-1 pr-6">
+          <div class="mb-1">QuarkXpress 2018 Pro</div>
+          <div class="text-grey text-xs mb-6">Love Print, Live Digital</div>
+          <div class="mb-8">
+            <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold">$399.99</span>
+          </div>
+          <div class="border-b border-grey-darker mb-10"></div>
+        </div>
+      </div>
+
+      <div class="w-1/2 flex pl-6">
+        <div class="w-24">
+          <img src="../assets/icon_pixelmator.png" alt="icon">
+        </div>
+        <div class="pl-6 flex-1">
+          <div class="mb-1">Pixelmator Pro</div>
+          <div class="text-grey text-xs mb-6">Pro image editing for everyone</div>
+          <div class="mb-8">
+            <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold">$39.99</span>
+          </div>
+          <div class="border-b border-grey-darker mb-10"></div>
+        </div>
+      </div>
+
+      <div class="w-1/2 flex">
+        <div class="w-24">
+          <img src="../assets/icon_omnifocus.png" alt="icon">
+        </div>
+        <div class="pl-6 flex-1 pr-6">
+          <div class="mb-1">OmniFocus 3</div>
+          <div class="text-grey text-xs mb-6">Accomplish More Every Day</div>
+          <div class="mb-8">
+            <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold uppercase">Get</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-1/2 flex pl-6">
+        <div class="w-24">
+          <img src="../assets/icon_fantastical2.png" alt="icon">
+        </div>
+        <div class="pl-6 flex-1">
+          <div class="mb-1">Fantastical 2</div>
+          <div class="text-grey text-xs mb-6">Calender and Reminders</div>
+          <div class="mb-8">
+            <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold">$69.99</span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div> <!-- end apps-games-we-love -->
+
+  <div class="border-b border-grey-darker mb-10"></div>
+
+  <div class="top-paid text-white">
+    <div class="flex justify-between">
+      <h2 class="font-normal mb-8">Top Paid</h2>
+      <a href="#" class="text-blue no-underline">See All</a>
+    </div>
+
+    <div>
+      <div class="flex">
+        <div class="w-1/2 pr-6">
+          <div class="flex mb-8">
+            <div><img src="../assets/icon_fcp.png" alt="icon" class="w-12"></div>
+            <div class="ml-3">1</div>
+            <div class="ml-3 flex flex-1 justify-between border-b border-grey-darker">
+              <div>
+                <div class="mb-1">Final Cut Pro</div>
+                <div class="text-grey text-xs mb-6">Professional post-production</div>
+              </div>
+              <div>
+                <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold">
+                  $399.99
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex mb-8">
+            <div><img src="../assets/icon_magnet.png" alt="icon" class="w-12"></div>
+            <div class="ml-3">2</div>
+            <div class="ml-3 flex flex-1 justify-between border-b border-grey-darker">
+              <div>
+                <div class="mb-1">Magnet</div>
+                <div class="text-grey text-xs mb-6">Organize Your Workspace</div>
+              </div>
+              <div>
+                <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold">
+                  $1.39
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex mb-8">
+            <div><img src="../assets/icon_daisydisk.png" alt="icon" class="w-12"></div>
+            <div class="ml-3">3</div>
+            <div class="ml-3 flex flex-1 justify-between">
+              <div>
+                <div class="mb-1">DaisyDisk</div>
+                <div class="text-grey text-xs mb-6">Recover disk space</div>
+              </div>
+              <div>
+                <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold uppercase">
+                  Open
+                </span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="w-1/2 pl-6">
+          <div class="flex mb-8">
+            <div><img src="../assets/icon_sims.png" alt="icon" class="w-12"></div>
+            <div class="ml-3">4</div>
+            <div class="ml-3 flex flex-1 justify-between border-b border-grey-darker">
+              <div>
+                <div class="mb-1">The Sims 2: Super Collection</div>
+                <div class="text-grey text-xs mb-6">Your faves in one place</div>
+              </div>
+              <div>
+                <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold">
+                  $39.99
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex mb-8">
+            <div><img src="../assets/icon_word.png" alt="icon" class="w-12"></div>
+            <div class="ml-3">5</div>
+            <div class="ml-3 flex flex-1 justify-between border-b border-grey-darker">
+              <div>
+                <div class="mb-1">Word Document Writer Pro</div>
+                <div class="text-grey text-xs mb-6">Business</div>
+              </div>
+              <div>
+                <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold">
+                  $39.99
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex mb-8">
+            <div><img src="../assets/icon_antivirusapp.png" alt="icon" class="w-12"></div>
+            <div class="ml-3">6</div>
+            <div class="ml-3 flex flex-1 justify-between">
+              <div>
+                <div class="mb-1">Antivirus Zap - Virus & Adware</div>
+                <div class="text-grey text-xs mb-6">Scan & Remove Malware & Adware</div>
+              </div>
+              <div>
+                <span class="bg-white text-blue rounded-full px-3 py-1 text-xs font-bold">
+                  $6.99
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div> <!-- end top-paid -->
+
+  <div class="border-b border-grey-darker mb-10"></div>
+
+  <div class="text-grey mb-10">
+    Terms & Conditions
   </div>
+
+</div>
 </template>
 
 <script>
