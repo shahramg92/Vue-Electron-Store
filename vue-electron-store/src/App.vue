77ea1042cfc5ca9a824c6748cc849747d4d6766a
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-19 11:59:33 -0600

    update

--- vue-electron-store/src/App.vue
+++ vue-electron-store/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" style="-webkit-app-region: drag" class="flex min-h-screen">
-    <div class="w-56 text-white bg-grey-darkest border-r border-blackblack fixed min-h-screen">
+    <div class="w-56 bg-grey-darkest text-white border-r border-blackblack fixed min-h-screen">
       This is a Sidebar
     </div>
     <HelloWorld msg="Welcome to Vue"/>
