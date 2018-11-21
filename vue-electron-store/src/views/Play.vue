752d5f24a620bd588ddaad0526ac7330e740ddaf
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-20 19:20:14 -0600

    update

--- vue-electron-store/src/views/Play.vue
+++ vue-electron-store/src/views/Play.vue
@@ -2,4 +2,25 @@
   <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
     <h1>This is the Play page</h1>
   </div>
+  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
+    <h1>This is the Play page</h1>
+  </div>
+  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
+    <h1>This is the Play page</h1>
+  </div>
+  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
+    <h1>This is the Play page</h1>
+  </div>
+  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
+    <h1>This is the Play page</h1>
+  </div>
+  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
+    <h1>This is the Play page</h1>
+  </div>
+  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
+    <h1>This is the Play page</h1>
+  </div>
+  <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
+    <h1>This is the Play page</h1>
+  </div>
 </template>
