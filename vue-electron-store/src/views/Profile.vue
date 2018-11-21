956daff37f15e0b38470000265817a450f702c64
Shahram Ghassemi <shahramg92@hotmail.com>
2018-11-21 17:33:30 -0600

    update

--- vue-electron-store/src/views/Profile.vue
+++ vue-electron-store/src/views/Profile.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="bg-black flex-1 ml-56 px-10 text-white pt-10">
-    <h1>This is the Profile page</h1>
+    <h1 class="ml-10">Hello!</h1>
+    <div class="border-b border-grey-darker mb-10 mx-10 mt-5"></div>
+    <p class="max-w leading-normal text-grey-lighter ml-10 mr-10">This app was created mainly for educational purposes. I wanted to use Vue.js and Electron together to create a desktop applicaiton which was something I had not done yet up until this point. I also wanted to implement a CSS framework called Tailwind CSS which excels in rapid UI development. All of the positioning of elements in this app was done using TailWind. My go-to for positioning elements is usually Flexbox so I wanted to try something new. I also made use of a third party package called Vue-Flickity which helped create the Carousel at the top of the Discover page. Overall, I am happy with this app and I feel it looks very similiar to the real app store. It is a little dynamic, but of course I could use an API instead of dummy data on the back end to pull data and populate the different areas of this app. I will probably not be doing that, simply because that would take very long and this app is just a demo app. Feel free to reach out to me at sha.ghassemi@gmail.com if you have any questions about this app or would like to know about my coding experience.</p>
   </div>
 </template>
